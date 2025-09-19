20a13f2861ab4e82427ec171abcbba912899672f
Joseph Nkuna <20115573+mjwnkuna@users.noreply.github.com>
2025-09-19 14:34:54 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "sandbox_2" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "om+saft02@jlinkedtech.onmicrosoft.com"
+    AccountName               = "sandbox-aft-2"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "om+saft02@jlinkedtech.onmicrosoft.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Owner" = "CloudTeam_SquadA"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "MJ"
+    change_reason       = "Company Demo Update"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
